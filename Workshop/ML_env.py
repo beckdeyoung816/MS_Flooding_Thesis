258acbb4747fe83858c0a92e95ba6aee2c614a0c
beckdeyoung816 <beckdeyoung@gmail.com>
2022-06-06 16:56:26 +0200

    Turned testing into script and added TCN

--- Workshop/ML_env.py
+++ Workshop/ML_env.py
@@ -9,11 +9,13 @@ import sys
 
 print(os.getcwd())
 os.chdir('/Users/beck/My Drive/VU/Thesis/Scripts/Workshop/')
-from CHNN import ANN
-from CHNN import to_learning
-from CHNN import performance
-from CHNN import model_run
-from CHNN import model_run_Beck
+# from CHNN import ANN
+# from CHNN import to_learning
+# from CHNN import performance
+# from CHNN import model_run as mr
+# from CHNN import model_run_Beck as mrB
+from CHNN import model_run_Beck_Script as mrB
+
 
 # parameters and variables
 station = 'cuxhaven-cuxhaven-germany-bsh'  # 'Cuxhaven' 'Hoek van Holland', Puerto Armuelles
@@ -23,21 +25,21 @@ variables = ['msl', 'grad', 'u10', 'v10', 'rho']  # 'grad', 'rho', 'phi', 'u10',
 tt_value = 0.67  # train-test value
 scaler = 'std_normal'  # std_normal, MinMax
 n_ncells = 0
-epochs = 50
+epochs = 25
 batch = 100
 batch_normalization = False
 neurons = 48
 filters = 8
 n_layers = 1  # now only works for uniform layers with same settings
 activation = 'relu'  # 'relu', 'swish', 'Leaky ReLu', 'sigmoid', 'tanh'
-loss = 'mae' #'mae'  # 'mae', 'mean_squared_logarithmic_error', 'mean_squared_error'
+loss = 'gumbel' #'mae'  # 'mae', 'mean_squared_logarithmic_error', 'mean_squared_error'
 optimizer = 'adam'  # SGD(lr=0.01, momentum=0.9), 'adam'
 dropout = True
 drop_value = 0.2
 l1, l2 = 0, 0.01
-ML = 'LSTM'  # 'LSTM', 'CNN', 'ConvLSTM', 'ANN', 'ALL', 'LSTM_TCN'
+ML = 'TCN'  # 'LSTM', 'CNN', 'ConvLSTM', 'ANN', 'ALL', 'LSTM_TCN'
 model_dir = os.path.join(os.getcwd(), 'Models')
-name_model = '{}_surge_ERA5'.format(ML)
+name_model = '{}_surge_ERA5_test'.format(ML)
 input_dir = 'Input_nc'
 output_dir = 'ML_model'
 figures_dir = 'Figures'
@@ -46,8 +48,8 @@ frac_ensemble = 0.5
 
 loop = 2
 
-logger, ch = model_run_Beck.set_logger(loop, n_ncells)
-model_run_Beck.ensemble(station, variables, ML, tt_value, input_dir, resample, resample_method, scaler,
+logger, ch = mrB.set_logger(loop, n_ncells)
+mrB.ensemble(station, variables, ML, tt_value, input_dir, resample, resample_method, scaler,
                    batch, n_layers, neurons, filters, dropout, drop_value, activation, optimizer,
                    batch_normalization, loss, epochs, loop=loop, n_ncells=n_ncells, l1=l1, l2=l2,
                    frac_ens=frac_ensemble, logger=logger, verbose=0, validation='select')
